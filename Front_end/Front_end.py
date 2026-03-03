fca8fd97d08c2d1a68766b5d3044378fb0b2e675
Shrisankhya Purandare <purandareshrisankhya0@gmail.com>
2026-03-03 16:40:34 +0530

    Add Django ecommerce app and frontend files (excluding secrets)

---
+++ Front_end/Front_end.py
@@ -0,0 +1,9 @@
+class front():
+    def __init__(self):
+        pass
+
+    def display(self):
+        print("Welcome to the Front End of the Application!")
+if __name__ == "__main__":
+    front_end = front()
+    front_end.display()
